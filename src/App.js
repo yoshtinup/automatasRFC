c5f55ff591e235359c58a9f98a2a168e95a15af1
GodKraken030804 <godkraken00@gmail.com>
2024-02-27 23:59:10 -0600

    termino

--- src/App.js
+++ src/App.js
@@ -7,48 +7,70 @@ import { Button } from 'primereact/button';
 import 'primereact/resources/themes/lara-light-indigo/theme.css';
 
 class Automaton {
-  constructor() {
-      this.states = [0, 1, 2, 3, 4];
-      this.final_states = [3];
-      this.alphabet = ['G', 'U', 'P', 'Y', 'g', 'u', 'p', 'y'];
-      this.transition_table = {
-          '0-g': 1, '1-g': 1, '1-u': 2, '2-u': 2, '2-p': 3, '3-p': 3, "3-y": 4, "4-y": 4,  
-          '0-G': 1, '1-G': 1, '1-U': 2, '2-U': 2, '2-P': 3, '3-P': 3, "3-Y": 4, "4-Y": 4
-      };
-      this.current_state = 0;
+    constructor() {
+        this.states = [0, 1, 2, 3, 4];
+        this.final_states = [3];
+        this.alphabet = ['G', 'U', 'P', 'Y', 'g', 'u', 'p', 'y'];
+        this.transition_table = {
+            
+            '0-g': 1, '0-G': 1, '1-g': 1, '1-G': 1,
+            '1-u': 2, '1-U': 2, '2-u': 2, '2-U': 2,
+            '2-p': 3, '2-P': 3, '3-p': 3, '3-P': 3,
+            '3-y': 4, '3-Y': 4, '4-y': 4, '4-Y': 4,
+            '1-u': 2, '1-U': 2, '1-p': 3, '1-P': 3, '1-y': 4, '1-Y': 4,
+            '2-g': 1, '2-G': 1, '2-p': 3, '2-P': 3, '2-y': 4, '2-Y': 4,
+            '3-g': 1, '3-G': 1, '3-u': 2, '3-U': 2, '3-y': 4, '3-Y': 4,
+            '4-g': 1, '4-G': 1, '4-u': 2, '4-U': 2, '4-p': 3, '4-P': 3
+        };
+        
+        this.current_state = 0;
+    }
+  
+    transition_to_state_with_input(input_value) {
+        const key = `${this.current_state}-${input_value}`;
+        if (!(key in this.transition_table)) {
+            this.current_state = null;
+        } else {
+            this.current_state = this.transition_table[key];
+        }
+    }
+  
+    in_accept_state() {
+        return this.final_states.includes(this.current_state);
+    }
+  
+    go_to_initial_state() {
+        this.current_state = 0;
+    }
+  
+    run_with_input_list(input_list) {
+        this.go_to_initial_state();
+        const highlightedLetters = [];
+        let gEncountered = false;
+    
+        for (const inp of input_list.toUpperCase()) { // Convertir la entrada a mayúsculas
+            this.transition_to_state_with_input(inp);
+            if (this.current_state === null) {
+                highlightedLetters.push({ letter: inp, isValid: false });
+                return { valid: false, highlightedLetters };
+            }
+            highlightedLetters.push({ letter: inp, isValid: true });
+    
+            if (inp === 'G') { // Ya no es necesario convertir a mayúsculas aquí
+                gEncountered = true;
+            }
+        }
+    
+        if (!gEncountered) {
+            return { valid: false, highlightedLetters };
+        }
+    
+        return { valid: this.in_accept_state(), highlightedLetters };
+    }
+    
   }
-
-  transition_to_state_with_input(input_value) {
-      const key = `${this.current_state}-${input_value}`;
-      if (!(key in this.transition_table)) {
-          this.current_state = null;
-      } else {
-          this.current_state = this.transition_table[key];
-      }
-  }
-
-  in_accept_state() {
-      return this.final_states.includes(this.current_state);
-  }
-
-  go_to_initial_state() {
-      this.current_state = 0;
-  }
-
-  run_with_input_list(input_list) {
-      this.go_to_initial_state();
-      const highlightedLetters = [];
-      for (const inp of input_list) {
-          this.transition_to_state_with_input(inp);
-          if (this.current_state === null) {
-              highlightedLetters.push({ letter: inp, isValid: false });
-              return { valid: false, highlightedLetters };
-          }
-          highlightedLetters.push({ letter: inp, isValid: true });
-      }
-      return { valid: this.in_accept_state(), highlightedLetters };
-  }
-}
+  
+  
 
 function DFAComponent() {
     const [inputValue, setInputValue] = useState('');
